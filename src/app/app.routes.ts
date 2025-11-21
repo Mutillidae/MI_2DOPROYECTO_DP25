eec6c99d38b45e85e1613d7d52053c512ae9e969
Yerkov Laime <gohanmiranda#@gmail.com>
2025-11-21 13:23:36 -0400

    gitpages2doPROYEdp25

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,8 +3,13 @@ import { Inicio } from './enlaces/inicio/inicio';
 import { Producto } from './enlaces/producto/producto';
 import { Formulario } from './enlaces/formulario/formulario';
 
+
+
+
 export const routes: Routes = [
     {path:"inicio", component: Inicio},
     {path:"producto", component: Producto},
-    {path:"formulario", component: Formulario}
+    {path:"formulario", component: Formulario},
+    
+
 ];
