a39df8ff8ca0be67af7409504f9b82c76dead1a6
Yerkov Laime <gohanmiranda#@gmail.com>
2025-11-20 20:25:45 -0400

    2DO_ProyectoDP25

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Inicio } from './enlaces/inicio/inicio';
+import { Producto } from './enlaces/producto/producto';
+import { Formulario } from './enlaces/formulario/formulario';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"inicio", component: Inicio},
+    {path:"producto", component: Producto},
+    {path:"formulario", component: Formulario}
+];
